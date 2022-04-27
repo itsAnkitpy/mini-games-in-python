3e9cc1101fc65d4a0192867b699b161821e9c3c7
itsAnkitpy <thegoat3585@gmail.com>
2022-04-27 23:00:24 +0530

    Created number guessing game

---
+++ guess_number.py
@@ -0,0 +1,56 @@
+import random
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+def set_difficulty():
+    level = input('Please select the difficulty level - Type easy or hard - ')
+    if level == 'easy':
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+def check_answer(guess,random_number,turns):
+    if guess > random_number:
+        print("Too High!!")
+        return turns - 1
+    elif guess <  random_number:
+        print("Too Low!!")
+        return turns - 1
+    else:
+        print(f'You guessed correctly. The selected number was {random_number}')
+        print('Thank you for playing.You can try some other games here as well')
+        print('\n')
+
+
+def guess_the_number(x):
+    print("Let's play Guess the Number")
+
+    random_number = random.randint(0,x)
+
+    print('\n')
+
+
+    # For setting up the difficulty level for this game
+    turns = set_difficulty()
+    print(f'You have {turns} attempts to guess the number.')
+    print('\n')
+
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 0 and {x}:  "))
+        
+        # Verifying if the guess is correct
+        turns = check_answer(guess,random_number,turns)
+        
+
+
+        if turns == 0:
+            print('You have ran out of attempts. YOU LOSE')
+        elif guess != random_number:
+            print(f'You have {turns} attempts to guess the number.')
+            print("Guess again")
+            print('\n')
+
+
+    
---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+os.system('cls')
+
+from guess_number import guess_the_number
+
+# More Games to be added
+while True:
+    txt = """Welcome to Mini Games!!!
+    - Guess The Number (1)
+    - Rock, Paper, Scissors (2)
+    - Wordle (3)
+    - ConnectFour (4)
+    - Tic Tac Toe (5)
+Select a game (press a number or 'q' to quit): """
+
+    value = input(txt)
+
+    if value == "1": 
+        upper_range = int(input("Please select the upper value range for this game - "))
+        print('\n')
+        guess_the_number(upper_range)
+
+    elif value == "q":
+        print('Thanks for playing Mini Games')
+        break
+        
